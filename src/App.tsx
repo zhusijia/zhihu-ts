83f18b170d280ed9b1462d72992a838db6b05c11
朱思嘉 <zhusijia@hotoa.com>
2018-10-09 00:01:06 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from 'antd';
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <Button type="primary">Button</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
